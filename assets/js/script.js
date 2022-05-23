03d2849c32618c6cba420754995ed65237d415c5
Eugene Durov <art.ewgen@gmail.com>
2022-05-23 11:47:43 +0300

    Update to new stylish version 2022

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,66 @@
-function toggleDiv(element){
+/*
+$(document).ready(function () {
+    if ($(window).width()>1000) {
+       /* $("#fullview").fullView({
+            //Navigation
+            navbar: "#navbar",
+            backToTop: true,
+            dots: false,
+      
+            // Callback
+            onScrollEnd: function (currentView) {
+                if (!currentView.hasClass('nav-transparent')) {
+                    $('.header').addClass('changed');
+                } else {
+                    $('.header').removeClass('changed');
+                }
+            }
+        })*/
 
-    var el = document.getElementById(element);
-    if(el.style.display != 'block')
-    {
-      el.style.display = 'block';
+/*
+        var fv = $("#fullview").fullView({
+            //Navigation
+            dots: true,
+            dotsPosition: "right",
+            dotsTooltips: true,
+        
+            //Scrolling
+            easing: "swing",
+            backToTop: false,
+        
+            // Accessibility
+            keyboardScrolling: true,
+        
+            // Callback
+            onScrollEnd: function (currentView, preView) {
+              console.log("Current", currentView);
+              console.log("Previus", preView);
+            }
+          });
+        
+          
+        
+          $("#select").change(function () {
+            // To Scroll to Specfic Section
+            fv.data("fullView").scrollTo($(this).val());
+          });
+
+          
+
+          
+
     }
-    else
-    {
-      el.style.display = '';
-    }
-  }
+
+});*/
+
+$(function() {
+  $('.scroll').on('click', function(e) {
+    e.preventDefault();
+    let pageHeight = window.innerHeight;
+    $('html, body').animate({ scrollTop: pageHeight}, 500, 'linear');
+  });
+
+  $('#header__burger').on('click', function(){
+    $('#header-nav-wrap').toggleClass('open');
+  });
+});

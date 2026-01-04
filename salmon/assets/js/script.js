751a8114301494ac4309bea62355225a4e22f134
Eugene Durov <art.ewgen@gmail.com>
2026-01-04 23:28:30 +0100

    removed eventlistener from Salmon

--- salmon/assets/js/script.js
+++ salmon/assets/js/script.js
@@ -670,38 +670,7 @@ $('#other-cases a').on('click', function(){
     ym(88891643,'reachGoal','clickOnCaseOtherCases');
 });
 
-
-function getClientId() {
-    const KEY = 'clientId';
-    let id = localStorage.getItem(KEY);
-    if (!id) {
-      id = crypto.randomUUID();
-      localStorage.setItem(KEY, id);
-    }
-    return id;
-  }
-  
-  async function trackPageView() {
-    const clientId = getClientId();
-  
-    try {
-      await fetch('http://localhost:4000/track/pageview', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          url: window.location.href,
-          title: document.title,
-          clientId,
-        }),
-      });
-    } catch (e) {
-      console.error('trackPageView error', e);
-    }
-  }
-  
-  window.addEventListener('load', trackPageView);
-  
-
+ 
 
 });
 
